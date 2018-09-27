54332f5ac98cdb9a39fe1505702ecbe42ea6d7d9
ricardofajin <ricardofajin@gmail.com>
2018-09-27 19:25:23 -0400

    v0.1 Funcao Scans TCP com saida em txt a melhorar

--- Core/Scan.py
+++ Core/Scan.py
@@ -1,12 +1,19 @@
 import nmap
+from colorama import Fore, Style
 
+def tcp(IP):
+    nm = nmap.PortScanner()
+    print(Fore.GREEN + 'Scaning ' + IP + Style.RESET_ALL)
+    # Tenho fe que a pasta estara la :D
+    #aguardando checkDir() - Utility
+    nm.scan(IP, '1-65535', '-sV -sC -T3 -o /results/' + IP.replace('/','-') + '.txt')
 
-def scanTCP(IP):
-    print('nmap TCP!')
-    print('IP: ' + IP)
+    print(nm.scaninfo())
+    print(nm.csv())
     return
 
-def scanUDP(IP):
+def udp(IP):
     print('nmap UDP!')
     print('IP: ' + IP)
+    # TODO deixa pra dps u.u
     return
