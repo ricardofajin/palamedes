bb0425483d38731a7fcb79f5be2896628894fb3e
ricardofajin <ricardofajin@gmail.com>
2018-09-27 17:28:01 -0400

    v0.0 Funcao Scans

--- Core/Scan.py
+++ Core/Scan.py
@@ -1,2 +1,12 @@
-def testando():
-    print('nmap!')
+import nmap
+
+
+def scanTCP(IP):
+    print('nmap TCP!')
+    print('IP: ' + IP)
+    return
+
+def scanUDP(IP):
+    print('nmap UDP!')
+    print('IP: ' + IP)
+    return
