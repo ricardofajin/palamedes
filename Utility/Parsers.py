865afdc2b7f78b3220ec6c98f1060df815342b76
danielchactoura <13952914+danielchactoura@users.noreply.github.com>
2018-09-28 15:49:03 -0300

    v0.2 Utility: Parsers.py - params added

--- Utility/Parsers.py
+++ Utility/Parsers.py
@@ -3,5 +3,6 @@ from argparse import ArgumentParser
 def getArgs():
     parser = ArgumentParser("palamedes.py")
     parser.add_argument("scopeFiles", nargs='+', help='Set the scope files')
-    parser.add_argument("--path", default='./results', help='Set the path to save the results (Default= ./results)')
+    parser.add_argument("-p", "--path", default='results', help='Set the path to save the results (Default= results)')
+    parser.add_argument("-e", "--interface", help='Set the network interface')
     return parser.parse_args()
