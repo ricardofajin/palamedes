281d9a4548b1a9b564859e3e73c141c3f07a7a55
ricardofajin <ricardofajin@gmail.com>
2018-10-01 18:19:48 -0400

    Make.py :TODO: Problema na criaç�a de pastas com espaço :\

--- Utility/Make.py
+++ Utility/Make.py
@@ -6,3 +6,5 @@ def dir(dir):
         os.mkdir(dir)
     except Exception as e:
         print('[' + Fore.RED + 'ERRO' + Style.RESET_ALL + '] - ' + str(e))
+
+# TODO: Problema em criar arquivos com espaço ex: "folder\ espace" ele n�o entende :( #TemQueVerIssoAe ...
