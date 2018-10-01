046fe2c96d3f7bee169ea2be3d05195ca95b7799
danielchactoura <13952914+danielchactoura@users.noreply.github.com>
2018-10-01 12:50:26 -0300

    v0.3 Core: Scan.py - error handling; Threads.py deleted

--- Core/Scan.py
+++ Core/Scan.py
@@ -11,16 +11,23 @@ def tcp(ip,folder,interface):
         Check.dir(folder)
     except:
         Make.dir('results')
+    try:
 
-    the_process = threading.Thread(name='Scan TCP - Palapica 8==D', target=nm.scan(ip, '1-65535', '-sV --open -sC -T3 -e ' + interface + ' -oN ' + folder + '/' + ip.replace('/', '-') + 'TCP_.txt'))
-    the_process.start()
+        the_process = threading.Thread(name='Scan TCP - Palapica 8==D', target=nm.scan(ip, '1-65535', '-sV --open -sC -T3 -e ' + interface + ' -oN ' + folder + '/' + ip.replace('/', '-') + 'TCP_.txt'))
+        the_process.start()
 
-    while the_process.isAlive():
-        Loading.animated()
+        while the_process.isAlive():
+            Loading.animated()
+    except Exception as e:
+        e = str(e)
+        e = e.replace("'", "")
+        e = e.replace("\\nQUITTING!\\n", "")
+        print("[" + Fore.RED + "ERROR" + Style.RESET_ALL + "]" " - " + e)
+        exit()
 
 def udp(ip,interface,folder='results'):
     nm = nmap.PortScanner()
-    print('[' + Fore.BLUE + 'UDP - SCANNING' + Style.RESET_ALL + '] - ' + IP)
+    print('[' + Fore.BLUE + 'UDP - SCANNING' + Style.RESET_ALL + '] - ' + ip)
 
     try:
         Check.dir(folder)
--- Core/Threads.py
+++
@@ -1,6 +0,0 @@
-from multiprocessing import Process
-
-def multi(vfunctions):
-    n = len(vfunctions)
-    for t in range(n):
-        Process(target=vfunctions[n]).start()
